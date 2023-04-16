79293fb2f8e0d43267acdae36e1e99401340b387
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-04-16 14:59:08 +0200

    ordenamos lista y una vex¡z ordenada la pedimos

--- parte1.py
+++ parte1.py
@@ -5,4 +5,9 @@ def lista_alumno_notas_y_asistencia():
     with open('calificaciones.csv', 'r') as f:
         reader = csv.reader(f)
         lista = []
-+
+        for row in reader:
+            lista.append(row)
+        lista.pop(0)
+        lista_ordenada = sorted(lista, key=lambda x: x[0])
+        return lista_ordenada
+
