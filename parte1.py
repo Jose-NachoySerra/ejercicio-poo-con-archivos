972b42af2b7c3d781261a9b27b4bd1a14efe8dac
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-04-16 14:57:49 +0200

    de nuevo, importamos csv

--- parte1.py
+++ parte1.py
@@ -1,10 +1,4 @@
 import csv
-#    def lista_alumno_notas_y_asistencia():
-       # with open('calificaciones.csv', 'r') as f:
-           # reader = csv.reader(f)
-            #for row in reader:
-                #print(row)
-    #lista_alumno_notas_y_asistencia()
-    #print(lista_alumno_notas_y_asistencia())
+
 
 
